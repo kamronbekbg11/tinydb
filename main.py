491d67a91b851ad04f4b5532ab4d0b8d7d5f14bc
Kamronbek BG <kamronbekbg@gmail.com>
2025-01-28 17:12:19 +0500

    the end

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
 from tinydb import TinyDB 
 import requests
 
-db = TinyDB("random.json")
+db = TinyDB("shop.json")
 
-url = "https://randommer.io/api/Name?nameType=firstname&quantity=10"
+shop = {
+    "iphone": [
+        {"name": "iphone 11", 
+        "model": "ios",
+        "memory": "64"
+        },
+        {"name": "iphone 12", 
+        "model": "ios",
+        "memory": "128"
+        },
+        {"name": "iphone 13", 
+        "model": "ios",
+        "memory": "256"
+        }
+    ],
 
-headers = {
-     "X-Api-Key": "29f760dcebe74d798990f7d51cd5e2b4"
+    "samsung": [
+        {"name": "samsung Galaxy S21", 
+        "model": "android",
+        "memory": "128"
+        },
+        {"name": "samsung Galaxy S21 Ultra", 
+        "model": "android",
+        "memory": "256"
+        },
+        {"name": "samsung Galaxy S22",
+        "model": "android",
+        "memory": "512"
+        }
+    ],
+
+    "vivo": [
+        {"name": "vivo Y93", 
+        "model": "android",
+        "memory": "64"
+        },
+        {"name": "vivo Y95", 
+        "model": "android",
+        "memory": "128"
+        },
+        {"name": "vivo Y96", 
+        "model": "android",
+        "memory": "256"
+        }
+    ]
 }
-response  = requests.get(url,headers=headers)
-r = response.json()
-for user in r:
-    db.insert({
-        "name": f"{user}"
-    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# db = TinyDB("random.json")
+
+# url = "https://randommer.io/api/Name?nameType=firstname&quantity=10"
+
+# headers = {
+#      "X-Api-Key": "29f760dcebe74d798990f7d51cd5e2b4"
+# }
+# response  = requests.get(url,headers=headers)
+# r = response.json()
+# for user in r:
+#     db.insert({
+#         "name": f"{user}"
+#     })
